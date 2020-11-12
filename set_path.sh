8c83a7daa7779a9742e4bdcfaa6b76849abfc4e7
Ionel Gog <gogionel@gmail.com>
2020-11-11 19:13:15 -0800

    Updated the set path script to not require a CARLA_VERSION variable.

--- set_path.sh
+++ set_path.sh
@@ -5,20 +5,8 @@ if [ -z "$CARLA_HOME" ]; then
     exit 1
 fi
 
-# Get the carla version.
-CARLA_VERSION=$(cat $CARLA_HOME/VERSION)
-if [ -z "$CARLA_VERSION" ]; then
-    echo "Please set \$CARLA_VERSION before sourcing this script"
-    exit 1
-fi
-
 export ROOT_SCENARIO_RUNNER=`pwd`
 export CARLA_SERVER=${CARLA_HOME}/CarlaUE4.sh
+CARLA_EGG=$(ls $CARLA_HOME/PythonAPI/carla/dist/carla*py3*egg)
 
-if [ $CARLA_VERSION = "0.9.9" ] ; then
-    PYTHON_VER=3.7
-else
-    PYTHON_VER=3.5
-fi
-
-export PYTHONPATH=${CARLA_HOME}/PythonAPI/carla/dist/carla-$CARLA_VERSION-py$PYTHON_VER-linux-x86_64.egg:${CARLA_HOME}/PythonAPI:${CARLA_HOME}/PythonAPI/carla/:${CARLA_HOME}/PythonAPI/carla/agents/:${ROOT_SCENARIO_RUNNER}/
+export PYTHONPATH=$CARLA_EGG:${CARLA_HOME}/PythonAPI:${CARLA_HOME}/PythonAPI/carla/:${CARLA_HOME}/PythonAPI/carla/agents/:${ROOT_SCENARIO_RUNNER}/
